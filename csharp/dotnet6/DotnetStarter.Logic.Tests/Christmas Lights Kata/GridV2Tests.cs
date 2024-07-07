c3b2c1d7e8fef6b68f9255cfd4751263be11adc7
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-07 20:52:51 +0530

    Refactor GridV2Tests to use GridFactory methods
    Updated test cases in GridV2Tests.cs to use CreateLightGridV2 from GridFactory. Modified assertions in TestTurnOn, TestTurnOff, TestToggle, and TestHowManyLightsAreOn for clarity and correctness.

--- csharp/dotnet6/DotnetStarter.Logic.Tests/Christmas Lights Kata/GridV2Tests.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/Christmas Lights Kata/GridV2Tests.cs
@@ -9,7 +9,7 @@ namespace GridTests
         [Fact]
         public void TestCtor()
         {
-            var grid = GridFactory.CreateLightGrid(1000, 1000);
+            var grid = GridFactory.CreateLightGridV2(1000, 1000);
             Assert.Equal(1000000, grid.GetLightCount());
             Assert.Equal(0, grid.GetTotalBrightness());
         }
@@ -22,13 +22,13 @@ namespace GridTests
         [InlineData(499, 499, 500, 500, 4)]
         public void TestTurnOn(int x1, int y1, int x2, int y2, int expected)
         {
-            var grid = GridFactory.CreateLightGrid(1000, 1000);
+            var grid = GridFactory.CreateLightGridV2(1000, 1000);
             Tuple<int, int> startPosition = new(x1, y1), endPosition = new(x2, y2);
             grid.TurnOn(startPosition, endPosition);
             Assert.Equal(expected, grid.GetTotalBrightness());
 
             grid.TurnOn(startPosition, endPosition);
-            Assert.Equal(expected * 2, grid.GetTotalBrightness());
+            Assert.Equal(expected + expected, grid.GetTotalBrightness());
         }
 
         [Theory]
@@ -38,7 +38,7 @@ namespace GridTests
         [InlineData(499, 499, 500, 500, 4)]
         public void TestTurnOff(int x1, int y1, int x2, int y2, int expected)
         {
-            var grid = GridFactory.CreateLightGrid(1000, 1000);
+            var grid = GridFactory.CreateLightGridV2(1000, 1000);
             Tuple<int, int> startPosition = new(x1, y1),
                 endPosition = new(x2, y2),
                 firstPosition = new(0, 0),
@@ -49,21 +49,23 @@ namespace GridTests
 
             grid.TurnOn(firstPosition, lastPosition);
             grid.TurnOff(firstPosition, lastPosition);
-            Assert.Equal(0, grid.GetTotalBrightness());
+            Assert.Equal(1000000 - 1000000, grid.GetTotalBrightness());
 
             grid.TurnOn(firstPosition, lastPosition);
             grid.TurnOn(firstPosition, lastPosition);
             grid.TurnOff(firstPosition, lastPosition);
-            Assert.Equal(1000000, grid.GetTotalBrightness());
+            Assert.Equal(1000000 + 1000000 - 1000000, grid.GetTotalBrightness());
 
             grid.TurnOn(firstPosition, lastPosition);
             grid.TurnOff(startPosition, endPosition);
-            Assert.Equal(1000000 - expected, grid.GetTotalBrightness());
+            Assert.Equal((1000000) + (1000000 - expected), grid.GetTotalBrightness());
 
             grid.TurnOn(startPosition, endPosition);
             grid.TurnOn(startPosition, endPosition);
+            Assert.Equal((1000000) + (1000000 - expected) + (expected * 2), grid.GetTotalBrightness());
+            
             grid.TurnOff(startPosition, endPosition);
-            Assert.Equal(expected, grid.GetTotalBrightness());
+            Assert.Equal((1000000) + (1000000 - expected) + expected, grid.GetTotalBrightness());
         }
 
         [Theory]
@@ -73,13 +75,13 @@ namespace GridTests
         [InlineData(0, 0, 0, 0, 1)]
         public void TestToggle(int x1, int y1, int x2, int y2, int expected)
         {
-            var grid = GridFactory.CreateLightGrid(1000, 1000);
+            var grid = GridFactory.CreateLightGridV2(1000, 1000);
             Tuple<int, int> startPosition = new(x1, y1), endPosition = new(x2, y2), firstPosition = new(0, 0), lastPosition = new(999, 999);
             grid.Toggle(startPosition, endPosition);
             Assert.Equal(expected * 2, grid.GetTotalBrightness());
 
             grid.Toggle(firstPosition, lastPosition);
-            Assert.Equal((expected * 4) + ((1000000 - expected) * 2), grid.GetTotalBrightness());
+            Assert.Equal((expected * 2) + (1000000 * 2), grid.GetTotalBrightness());
         }
 
         // exactly same as TestTurnOn
@@ -90,7 +92,7 @@ namespace GridTests
         [InlineData(0, 0, 0, 0, 1)]
         public void TestHowManyLightsAreOn(int x1, int y1, int x2, int y2, int expected)
         {
-            var grid = GridFactory.CreateLightGrid(1000, 1000);
+            var grid = GridFactory.CreateLightGridV2(1000, 1000);
             Tuple<int, int> startPosition = new(x1, y1), endPosition = new(x2, y2);
 
             grid.TurnOn(startPosition, endPosition);
@@ -98,7 +100,7 @@ namespace GridTests
 
             grid.TurnOn(startPosition, endPosition);
             grid.TurnOn(startPosition, endPosition);
-            Assert.Equal(expected * 2, grid.GetTotalBrightness());
+            Assert.Equal(expected + expected * 2, grid.GetTotalBrightness());
         }
     }
 }
