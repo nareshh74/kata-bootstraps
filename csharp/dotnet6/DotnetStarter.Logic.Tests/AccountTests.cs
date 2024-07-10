902877625cf1e016081d443b3372b9bb4f755a6c
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-11 00:45:37 +0530

    withdraw test

--- csharp/dotnet6/DotnetStarter.Logic.Tests/AccountTests.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/AccountTests.cs
@@ -21,5 +21,22 @@ namespace BankingKataTests
             Assert.Equal(amount, transactions[0].Amount);
             Assert.Equal(TransactionType.Credit, transactions[0].Type);
         }
+
+        [Fact]
+        public void Withdraw_ShouldRecordExpectedTransaction()
+        {
+            // Arrange
+            var account = new Account();
+            var amount = 100;
+
+            // Act
+            account.Withdraw(amount);
+
+            // Assert
+            var transactions = account.GetTransactions();
+            Assert.Single(transactions);
+            Assert.Equal(amount, transactions[0].Amount);
+            Assert.Equal(TransactionType.Debit, transactions[0].Type);
+        }
     }
 }
