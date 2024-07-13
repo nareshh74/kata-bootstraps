96926c985fa8ad52dbae3a01896d6fc7efcfcbe6
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-14 01:00:02 +0530

    organize tests by public APIs

--- csharp/dotnet6/DotnetStarter.Logic.Tests/Bowling Game Kata/GameTests.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/Bowling Game Kata/GameTests.cs
@@ -6,49 +6,61 @@ namespace BowlingGameTests
 {
     public class GameTests
     {
-        [Fact]
-        public void New_Game_Should_Have_atleast_1_Frame()
+        public class Ctor
         {
-            var game = new Game();
-            Assert.Single(game.Frames);
-        }
-
-        [Fact]
-        public void Should_Start_Next_Frame_After_2_Rolls()
-        {
-            var game = new Game();
-            game.Roll(5);
-            game.Roll(4);
-            Assert.Equal(2, game.Frames.Count);
-        }
-
-        [Fact]
-        public void Should_Complete_After_10_Frames()
-        {
-            var game = new Game();
-            for (int i = 0; i < 18; i++)
+            [Fact]
+            public void Should_Have_atleast_1_Frame()
             {
-                game.Roll(4);
+                var game = new Game();
+                Assert.Single(game.Frames);
             }
-            Assert.True(game.IsComplete());
         }
 
-        [Fact]
-        public void Should_Not_Support_Roll_After_Game_Completes()
+        public class Roll
         {
-            var game = new Game();
-            for (int i = 0; i < 18; i++)
+            [Fact]
+            public void Should_Start_Next_Frame_After_2_Rolls()
             {
+                var game = new Game();
+                game.Roll(5);
                 game.Roll(4);
+                Assert.Equal(2, game.Frames.Count);
             }
-            Assert.Throws<InvalidOperationException>(() => game.Roll(4));
+
+            [Fact]
+            public void Should_Throw_Roll_After_Game_Completes()
+            {
+                var game = new Game();
+                for (int i = 0; i < 18; i++)
+                {
+                    game.Roll(4);
+                }
+                Assert.Throws<InvalidOperationException>(() => game.Roll(4));
+            }
         }
 
-        [Fact]
-        public void GetScore_Should_Throw_If_Game_not_Complete()
+        public class IsComplete
         {
-            var game = new Game();
-            Assert.Throws<InvalidOperationException>(() => game.GetScore());
+            [Fact]
+            public void Should_Complete_After_10_Frames()
+            {
+                var game = new Game();
+                for (int i = 0; i < 18; i++)
+                {
+                    game.Roll(4);
+                }
+                Assert.True(game.IsComplete());
+            }
+        }
+
+        public class GetScore
+        {
+            [Fact]
+            public void Should_Throw_If_Game_not_Complete()
+            {
+                var game = new Game();
+                Assert.Throws<InvalidOperationException>(() => game.GetScore());
+            }
         }
     }
 
