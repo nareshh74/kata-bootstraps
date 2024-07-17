6fcbc6844e5f373796a020dd2ad9d0fa37df8288
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-18 00:08:03 +0530

    move class roll

--- csharp/dotnet6/DotnetStarter.Logic/Bowling Game Kata/Game.cs
+++ csharp/dotnet6/DotnetStarter.Logic/Bowling Game Kata/Game.cs
@@ -120,19 +120,4 @@ namespace BowlingGameKata
             return this.Rolls.First().GetScore();
         }
     }
-
-    public class Roll
-    {
-        public int KnockedPinCount { get; }
-
-        public Roll(int knockedPinCount)
-        {
-            this.KnockedPinCount = knockedPinCount;
-        }
-
-        public int GetScore()
-        {
-            return this.KnockedPinCount;
-        }
-    }
 }
