d979b76c8de80e63da03808d42c37907ecc55577
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-18 00:08:52 +0530

    move class frame

---
+++ csharp/dotnet6/DotnetStarter.Logic/Bowling Game Kata/Frame.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingGameKata;
+
+public class Frame
+{
+    private readonly int _index;
+    public List<Roll> Rolls { get; }
+
+    public Frame(int index)
+    {
+        this._index = index;
+        this.Rolls = new();
+    }
+
+    public void Roll(int knockedPinCount)
+    {
+        this.Rolls.Add(new Roll(knockedPinCount));
+    }
+
+    public bool IsComplete()
+    {
+        // 10th frame
+        if (this._index == 9)
+        {
+            if (this.GetScore() >= 10)
+            {
+                return this.Rolls.Count == 3;
+            }
+            return this.Rolls.Count == 2;
+        }
+
+        return this.Rolls.First().GetScore() == 10 // strike/spare
+               || this.Rolls.Count == 2; // normal case
+    }
+
+    public int GetScore()
+    {
+        return this.Rolls.Sum(roll => roll.GetScore());
+    }
+
+    public int GetFirstRollScore()
+    {
+        return this.Rolls.First().GetScore();
+    }
+}
