e95af0c1e85ac99e9436ea8ec6b0cd8aba4e77d2
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-31 23:46:07 +0530

    more cases - end of requirement 1

--- csharp/dotnet6/DotnetStarter.Logic.Tests/ATM/ATMTests.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/ATM/ATMTests.cs
@@ -9,7 +9,13 @@ public class AtmTests
 {
     [Theory]
     [InlineData(100, "1 bill of 100.")]
-    [InlineData(200, "2 bills of 100.")]
+    [InlineData(50, "1 bill of 50.")]
+    [InlineData(150, @"1 bill of 100.
+1 bill of 50.")]
+    [InlineData(434, @"2 bills of 200.
+1 bill of 20.
+1 bill of 10.
+2 coins of 2.")]
     public void Atm_should_display_expected_result_after_withdrawal(int withdrawalAmount, string expected)
     {
         using StringWriter sw = new();
