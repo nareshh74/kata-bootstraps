1986436fa63ce1e15f4aedf50cdb5016981f2d98
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-31 23:49:05 +0530

    nit

--- csharp/dotnet6/DotnetStarter.Logic.Tests/ATM/ATMTests.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/ATM/ATMTests.cs
@@ -1,29 +1,32 @@
-using System.IO;
 using System;
+using System.IO;
 using DotnetStarter.Logic.ATM;
 using Xunit;
 
-namespace ATMTests;
+namespace DotnetStarter.Logic.Tests.ATM;
 
-public class AtmTests
+public class AtmShould
 {
-    [Theory]
-    [InlineData(100, "1 bill of 100.")]
-    [InlineData(50, "1 bill of 50.")]
-    [InlineData(150, @"1 bill of 100.
+    public class WithdrawShould
+    {
+        [Theory]
+        [InlineData(100, "1 bill of 100.")]
+        [InlineData(50, "1 bill of 50.")]
+        [InlineData(150, @"1 bill of 100.
 1 bill of 50.")]
-    [InlineData(434, @"2 bills of 200.
+        [InlineData(434, @"2 bills of 200.
 1 bill of 20.
 1 bill of 10.
 2 coins of 2.")]
-    public void Atm_should_display_expected_result_after_withdrawal(int withdrawalAmount, string expected)
-    {
-        using StringWriter sw = new();
-        Console.SetOut(sw);
+        public void Atm_should_display_expected_result_after_withdrawal(int withdrawalAmount, string expected)
+        {
+            using StringWriter sw = new();
+            Console.SetOut(sw);
 
-        AtmClient.WithDraw(withdrawalAmount);
+            AtmClient.WithDraw(withdrawalAmount);
 
-        var actual = sw.ToString().Trim();
-        Assert.True(expected.Equals(actual), $"expected - {expected} | actual - {actual}");
+            var actual = sw.ToString().Trim();
+            Assert.True(expected.Equals(actual), $"expected - {expected} | actual - {actual}");
+        }
     }
 }
