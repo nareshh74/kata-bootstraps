d6fcc28956da76cd580c2eb47967c9267c78c3f9
Naresh Kumar <kumarnaresh@microsoft.com>
2024-08-25 18:03:50 +0530

    nit - organize tests refactor

--- csharp/dotnet6/DotnetStarter.Logic.Tests/ParkingLotService/ParkingLotShould.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/ParkingLotService/ParkingLotShould.cs
@@ -5,50 +5,53 @@ namespace DotnetStarter.Logic.Tests.ParkingLotService
 {
     public class ParkingLotShould
     {
-        [Fact]
-        public void Allow_to_park_car_and_return_ticket_as_expected()
+        public class ParkShould
         {
-            // Arrange
-            var parkingLot = new ParkingLot("PR1234", 1, 1);
-            var vehicle = new Vehicle("ABC123", VehicleType.Truck, "Black");
-            
-            // Act
-            var ticket = parkingLot.Park(vehicle);
+            [Fact]
+            public void Allow_to_park_car_and_return_ticket_as_expected()
+            {
+                // Arrange
+                var parkingLot = new ParkingLot("PR1234", 1, 1);
+                var vehicle = new Vehicle("ABC123", VehicleType.Truck, "Black");
 
-            // Assert
-            Assert.NotNull(ticket);
-            Assert.Equal("PR1234_1_1", ticket.Id);
-        }
+                // Act
+                var ticket = parkingLot.Park(vehicle);
 
-        [Fact]
-        public void Not_allow_to_park_car_when_no_slot_available()
-        {
-            // Arrange
-            var parkingLot = new ParkingLot("PR1234", 1, 1);
-            var vehicle = new Vehicle("XYZ123", VehicleType.Truck, "Black");
-            parkingLot.Park(new Vehicle("ABC123", VehicleType.Truck, "Black"));
+                // Assert
+                Assert.NotNull(ticket);
+                Assert.Equal("PR1234_1_1", ticket.Id);
+            }
 
-            // Act
-            var ticket = parkingLot.Park(vehicle);
+            [Fact]
+            public void Not_allow_to_park_car_when_no_slot_available()
+            {
+                // Arrange
+                var parkingLot = new ParkingLot("PR1234", 1, 1);
+                var vehicle = new Vehicle("XYZ123", VehicleType.Truck, "Black");
+                parkingLot.Park(new Vehicle("ABC123", VehicleType.Truck, "Black"));
 
-            // Assert
-            Assert.Equal(ticket.ToString(), Ticket.SlotFull.ToString());
-        }
+                // Act
+                var ticket = parkingLot.Park(vehicle);
 
-        [Theory]
-        [InlineData(VehicleType.Car)]
-        [InlineData(VehicleType.Bike)]
-        public void Not_allow_to_park_car_when_no_slot_available_for_vehicle_type(VehicleType vehicleType)
-        {
-            // Arrange
-            var parkingLot = new ParkingLot("PR1234", 1, 1);
-            var vehicle = new Vehicle("XYZ123", vehicleType, "Black");
+                // Assert
+                Assert.Equal(ticket.ToString(), Ticket.SlotFull.ToString());
+            }
 
-            // Act
-            var ticket = parkingLot.Park(vehicle);
+            [Theory]
+            [InlineData(VehicleType.Car)]
+            [InlineData(VehicleType.Bike)]
+            public void Not_allow_to_park_car_when_no_slot_available_for_vehicle_type(VehicleType vehicleType)
+            {
+                // Arrange
+                var parkingLot = new ParkingLot("PR1234", 1, 1);
+                var vehicle = new Vehicle("XYZ123", vehicleType, "Black");
 
-            // Assert
-            Assert.Equal(ticket.ToString(), Ticket.SlotFull.ToString());
+                // Act
+                var ticket = parkingLot.Park(vehicle);
+
+                // Assert
+                Assert.Equal(ticket.ToString(), Ticket.SlotFull.ToString());
+            }
         }
     }
 }
